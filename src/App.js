1018636c9e0a4870ded0f21467aa55e1f24d9fc3
shaik apsar <apsarsk823@gmail.com>
2023-01-20 23:51:49 +0530

    Login form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { LoginForm } from './components/auth/LoginForm';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+      <LoginForm />
+    </LocalizationProvider>
   );
 }
 
